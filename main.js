2d71422fd08c98720343bb408cddf9461273c225
chenzhutao <1248378683@qq.com>
2018-03-14 21:36:13 +0800

    mvc

--- main.js
+++ main.js
@@ -1,38 +1,121 @@
 fakeData()
 
-$('#app').on('click','#addOne',function(){
-  var oldNumber = $('#number').text()
-  var newNumber = oldNumber-0+1
-  axios.put('/book/1',{
-    number:newNumber
-  }).then(()=>{
-  $('#number').text(newNumber)
+function Model(options){
+  this.data = options.data
+  this.resource = options.resource
+}
+Model.prototype.fetch = function(id){
+  return axios.get(`/${this.resource}s/${id}`).then((response)=>{
+    this.data = response.data
+    return response
+    
   })
-})
+}
 
-$('#app').on('click','#minusOne',function(){
-  var oldNumber = $('#number').text()
-  if(oldNumber>0){
-    var newNumber = oldNumber-0-1
-    axios.put('/book/1',{
-      number:newNumber
-    }).then(()=>{
-    $('#number').text(newNumber)
-    })
-  }else{
-    alert('没有库存了')
+Model.prototype.update = function(data){
+  let id = this.data.id
+  return axios.put(`/${this.resource}s/${id}`,data).then((response)=>{
+    this.data = response.data
+      console.log('response')
+      console.log(response.data)
+    return response
+    
+  })
+}
+
+function View({el,template}){
+  this.el = el
+  this.template = template
+}
+View.prototype.render = function(data){
+
+  let html = this.template
+  for(let key in data){
+    html = html.replace(`__${key}__`,data[key])
   }
+  $(this.el).html(html)
+
+}
+
+
+// ----------  上面是 MVC 类，下面是对象
+let model = new Model({
+  data:{
+    name: '',
+    number: 0,
+    id: ''
+  },
+  resource:'book'
 })
-$('#app').on('click','#reset',function(){
-  axios.put('/book/1',{
-    number:newNumber
-  }).then(()=>{
-  $('#number').text(0)
-  })
+
+let view = new View({
+  el: '#app',
+  template: `
+    <div>
+    书名：《__name__》
+    数量：<span id=number>__number__</span>
+    </div>
+    <div>
+      <button id="addOne">加1</button>
+      <button id="minusOne">减1</button>
+      <button id="reset">归零</button>
+    </div>
+  `
 })
 
 
+var controller = {
+  init(options){
+    this.view = options.view
+    this.model = options.model
+    this.view = view 
+    this.model = model
+    
+    this.view.render(this.model.data)
+    this.bindEvents()
+    this.model.fetch(1).then(()=>{
+      this.view.render(this.model.data)
+    })
+  },
+  addOne(){
+    let oldNumber = $('#number').text()
+    let newNumber = oldNumber-0+1
+    this.model.update({
+      number: newNumber
+    }).then(()=>{
+      this.view.render(this.model.data)
+      
+    })
+  },
+  minusOne(){
+    let oldNumber = $('#number').text()
+      if(oldNumber>0){
+      let newNumber = oldNumber-0-1
+      this.model.update({
+        number:newNumber
+      }).then(()=>{
+        this.view.render(this.model.data)
+      })      
+    }else{
+      alert('没有库存了')
+    }
 
+  },
+  reset(){
+    this.model.update({
+      number:0
+    }).then(()=>{
+      this.view.render(this.model.data)
+    })
+  },
+  bindEvents(){
+    $(this.view.el).on('click','#addOne',this.addOne.bind(this))
+    $(this.view.el).on('click','#minusOne',this.minusOne.bind(this))
+    $(this.view.el).on('click', '#reset', this.reset.bind(this))
+  }
+  
+}
+controller.init({view:view,model:model})
 
 //不要看
 
@@ -49,22 +132,17 @@ function fakeData(){
     // let config = response.config
     // console.log(config)
     // let {method,url,data} = config //data 是请求的data 
-    let{config:{method,url,config}} = response
+    let{config:{method,url,data}} = response
     if(url ==='/books/1' && method ==='get'){
       response.data = book
     }else if(url ==='/books/1' && method ==='put'){
-      Objec.assign(book,data)
+      //将字符串data转化为数字
+      
+      data = JSON.parse(data)
+      Object.assign(book,data)
+      
       response.data = book
     }
     return response;
-  });
-  // 创建请求
-
-  axios.get('/books/1')
-    .then(({data})=> {
-      let orginalHtml = $('#app').html()
-      let newHtml = orginalHtml.replace('__name__',data.name)
-      .replace('__number__',data.number)
-      $('#app').html(newHtml)
   });
 }
