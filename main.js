aedd62c16ae36a3e50d31fb59ad93b24b321f3ca
chenzhutao <1248378683@qq.com>
2018-03-14 14:43:47 +0800

    使用axios

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
-$('#addOne').on('click',function(){
-    var oldNumber = $('#number').text()
-    var newNumber = oldNumber-0+1
-    $('#number').text(newNumber)
-  })
-
-  $('#minusOne').on('click',function(){
-    var oldNumber = $('#number').text()
-    if(oldNumber>0){
-      var newNumber = oldNumber-0-1
-      $('#number').text(newNumber)
-     
-    }else{
-      alert('没有库存了')
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  // let config = response.config
+  // console.log(config)
+  // let {method,url,data} = config //data 是请求的data 
+  let{config:{method,url,config}} = response
+  if(url ==='/books/1' && method ==='get'){
+    response.data = {
+      'name':'JavaScript高级程序设计',
+      number:1,
+      id:1
     }
-  })
-  
-  $('#reset').on('click',function(){
-    $('#number').text(0)
-  })
+  }
+  return response;
+});
+// 创建请求
+
+axios.get('/books/1')
+.then(({data})=> {
+  let orginalHtml = $('#app').html()
+  let newHtml = orginalHtml.replace('__name__',data.name)
+  .replace('__number__',data.number)
+  $('#app').html(newHtml)
+});
+
+$('#app').on('click','#addOne',function(){
+var oldNumber = $('#number').text()
+var newNumber = oldNumber-0+1
+$('#number').text(newNumber)
+})
+
+$('#app').on('click','#minusOne',function(){
+var oldNumber = $('#number').text()
+if(oldNumber>0){
+  var newNumber = oldNumber-0-1
+  $('#number').text(newNumber)
+}else{
+  alert('没有库存了')
+}
+})
+$('#app').on('click','#reset',function(){
+$('#number').text(0)
+})
